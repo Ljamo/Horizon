a1cdb7c65b6c82e4649077eb5bbb8af522bc1447
Ljamo <masterluka21@gmail.com>
2022-12-20 21:04:18 +1100

    Added input polling.

--- premake5.lua
+++ premake5.lua
@@ -71,7 +71,7 @@ project "Horizon"
 
 		postbuildcommands
 		{
-			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+			("{COPY} %{cfg.buildtarget.relpath} \"../bin/" .. outputdir .. "/Sandbox/\"")
 		}
 
 	filter "configurations:Debug"
